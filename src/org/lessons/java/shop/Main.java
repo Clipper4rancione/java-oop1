60497f35acfb2fb9de3a8964bbd85ec33a419fe9
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-10 16:12:05 +0100

    [add] printed object

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -3,7 +3,13 @@ package org.lessons.java.shop;
 public class Main {
     public static void main(String[] args) {
 
-        Product firstProduct = new Product("Red Bull", "Energy Drink", 2.00, 0);
+        Product firstProduct = new Product("Red Bull", "Energy Drink", 2.00, 0.2);
+        System.out.println("Nome: " + firstProduct.productName);
+        System.out.println("Descrizione: " + firstProduct.productDescription);
+        System.out.println("Prezzo: " + firstProduct.price);
+        System.out.println("Prezzo comprensivo di iva: " + firstProduct.priceWithIva());
+        System.out.println("iva: " + firstProduct.iva);
+
 
     }
 }
