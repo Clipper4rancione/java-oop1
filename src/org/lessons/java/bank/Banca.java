da1b804071d1ba46b1a3bd5e0f8f2ce79fcad71d
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-10 17:58:40 +0100

    [add] printed initial info

--- src/org/lessons/java/bank/Banca.java
+++ src/org/lessons/java/bank/Banca.java
@@ -3,5 +3,14 @@ package org.lessons.java.bank;
 public class Banca {
     public static void main(String[] args) {
 
+        // Grazie per lo spunto del ciclo WHILE!
+
+        Conto myAccount = new Conto("Ciro Meluziis");
+        System.out.println("*************");
+        System.out.println("Benvenuto nel tuo account " + myAccount.getUserName() + "!");
+        System.out.println("*************");
+        System.out.println();
+        System.out.println("Il tuo saldo disponibile è di: " + myAccount.formatBalance() + "€");
+
     }
 }
