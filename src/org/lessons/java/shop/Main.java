a53cf5eea960d044895520f6ebb4f46684fc68a5
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-10 16:25:35 +0100

    [add] getter in sout and push exercise 1 without bonus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         Product firstProduct = new Product("Red Bull", "Energy Drink", 2.00, 0.2);
-        System.out.println("Nome: " + firstProduct.productName);
-        System.out.println("Descrizione: " + firstProduct.productDescription);
-        System.out.println("Prezzo: " + firstProduct.price);
+        System.out.println("Nome: " + firstProduct.getProductName());
+        System.out.println("Descrizione: " + firstProduct.getProductDescription());
+        System.out.println("Nome: " + firstProduct.nameCoded());
+        System.out.println("Prezzo: " + firstProduct.getPrice());
         System.out.println("Prezzo comprensivo di iva: " + firstProduct.priceWithIva());
-        System.out.println("iva: " + firstProduct.iva);
+        System.out.println("iva: " + firstProduct.getIva());
 
 
     }
